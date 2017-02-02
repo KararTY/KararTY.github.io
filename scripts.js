6157779f7f9c9586cb8d43bbc5574ac7ea1a0987
KararTY <KararTY@users.noreply.github.com>
2017-02-02 22:34:49 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,36 @@
+var Chart = window.chart
+
+var ctx = document.getElementById('canvas')
+
+var data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: '"Noun"',
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+      data: [65, 59, 80, 81, 56, 55, 40],
+    }
+  ]
+}
+
+var myBarChart = new Chart(ctx, {
+  type: 'horizontalBar',
+  data: data,
+  options: options
+})
