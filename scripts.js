3c060ecf2d8f36627ce524921b2d4996640c078c
KararTY <KararTY@users.noreply.github.com>
2017-02-02 23:38:23 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,31 @@
 var ctx = document.getElementById('canvas')
 
+function request (opts, callback) {
+  var xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState != 4) return
+    if (xhr.status >= 200 && xhr.status < 400) {
+      return callback(null, xhr.getResponseHeader('Content-Type') != null && xhr.getResponseHeader('Content-Type').indexOf('application/json') != -1 ? JSON.parse(xhr.responseText) : xhr.responseText, xhr)
+    }
+    var msg = xhr.responseText || "Can't access"
+    callback(Error(msg), null, xhr)
+  }
+  xhr.open(opts.method || 'GET', opts.url)
+  for (var key in opts.headers) {
+    xhr.setRequestHeader(key, opts.headers[key])
+  }
+  xhr.send(opts.data)
+  return xhr
+}
+
+request({url: 'http://192.241.185.49/', method: 'GET'}, (err, res) => {
+  if (err) return console.log(err)
+  else {
+    var data = JSON.parse(res)
+    var topTenArray = []
+
+    points.sort(function(a, b){return b-a})
+    console.log(points)
 var data = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   datasets: [
@@ -22,13 +48,16 @@ var data = {
         'rgba(255, 159, 64, 1)'
       ],
       borderWidth: 1,
-      data: [65, 59, 80, 81, 56, 55, 40]
+      data: [1, 2, 3, 4, 5, 6, 7, 9, 10]
     }
   ]
 }
 
+  }
+}
+
 var myBarChart = new Chart(ctx, {
-  type: 'horizontalBar',
+  type: 'bar',
   data: data,
   options: {
     scales: {
