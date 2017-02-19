4eb8aa558821577d4e09436161f9b73246f649e2
KararTY <KararTY@users.noreply.github.com>
2017-02-19 19:04:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+'use strict'
+var ctx = document.getElementById('canvas')
+var XMLHttpRequest = XMLHttpRequest || {}
+var Chart = Chart || {}
+
+function request (opts, callback) {
+  var xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return
+    if (xhr.status >= 200 && xhr.status < 400) {
+      return callback(null, xhr.getResponseHeader('Content-Type') !== null && xhr.getResponseHeader('Content-Type').indexOf('application/json') !== -1 ? JSON.parse(xhr.responseText) : xhr.responseText, xhr)
+    }
+    var msg = xhr.responseText || "Can't access"
+    callback(Error(msg), null, xhr)
+  }
+  xhr.open(opts.method || 'GET', opts.url)
+  for (var key in opts.headers) {
+    xhr.setRequestHeader(key, opts.headers[key])
+  }
+  xhr.send(opts.data)
+  return xhr
+}
+function sortProperties (obj) {
+  // convert object into array
+  var sortable = []
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      sortable.push([key, obj[key]]) // each item is an array in format [key, value]
+    }
+  }
+  // sort items by value
+  sortable.sort(function (a, b) {
+    return b[1] - a[1] // compare numbers
+  })
+  return sortable // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
+}
+request({ url: 'http://192.241.185.49/', method: 'GET' }, (err, res) => {
+  // console.log('Running')
+  if (err) return console.log(err)
+  else {
+    // console.log(res)
+    // var topTenArray = []
+    res = sortProperties(res)
+    console.log(res)
+    var maximum = res.length
+    var minimum = 0
+    var arr = []
+    arr.push(Math.floor(Math.random() * (maximum - minimum + 1)) + minimum)
+    document.getElementById('randNoun').innerText = res[arr[0]][0]
+    document.getElementById('randNounMentioned').innerText = res[arr[0]][1]
+    console.log(arr)
+    var data = {
+      labels: [res[0][0], res[1][0], res[2][0], res[3][0], res[4][0], res[5][0], res[6][0], res[7][0], res[8][0], res[9][0]],
+      datasets: [
+        {
+          label: 'Mentioned',
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)'
+          ],
+          borderWidth: 1,
+          data: [res[0][1], res[1][1], res[2][1], res[3][1], res[4][1], res[5][1], res[6][1], res[7][1], res[8][1], res[9][1]]
+        }
+      ]
+    }
+    var myBarChart = new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          xAxes: [{
+            stacked: true
+          }],
+          yAxes: [{
+            stacked: true
+          }]
+        }
+      }
+    })
+  }
+})
+
--- pewdiegate.js
+++ pewdiegate.js
@@ -1,3 +1,6 @@
+'use strict'
+var XMLHttpRequest = XMLHttpRequest || {}
+
 function request (opts, callback) {
   var xhr = new XMLHttpRequest()
   xhr.onreadystatechange = function () {
@@ -19,7 +22,7 @@ function request (opts, callback) {
 var tbody = document.getElementById('tbody')
 
 request({ url: 'http://192.241.185.49/pewdiegate', method: 'GET' }, (err, res) => {
-  console.log('Running')
+  // console.log('Running')
   if (err) return console.log(err)
   else {
     var data = res.data
@@ -27,7 +30,10 @@ request({ url: 'http://192.241.185.49/pewdiegate', method: 'GET' }, (err, res) =
     for (var i = 0; i < data.length; i++) {
       var element = data[i]
 
+      // Create 'tr' that'll later be appended to tbody.
       var tr = document.createElement('tr')
+
+      // Create link row.
       var td = document.createElement('td')
       var a = document.createElement('a')
       a.setAttribute('href', element.link[0])
@@ -35,6 +41,7 @@ request({ url: 'http://192.241.185.49/pewdiegate', method: 'GET' }, (err, res) =
       td.appendChild(a)
       tr.appendChild(td)
 
+      // Create title row.
       var td2 = document.createElement('td')
       if (element.title.length > 30) {
         var titleText = element.title
@@ -44,24 +51,38 @@ request({ url: 'http://192.241.185.49/pewdiegate', method: 'GET' }, (err, res) =
       } else td2.innerText = element.title
       tr.appendChild(td2)
 
+      // Create date row.
       var td3 = document.createElement('td')
       td3.innerText = element.date
       tr.appendChild(td3)
 
+      // Create publication row.
       var td4 = document.createElement('td')
       td4.innerText = element.publication
       tr.appendChild(td4)
 
+      // Create author(s) row.
       var td5 = document.createElement('td')
       var authors = ''
+      // Loop through authors.
       for (var o = 0; o < element.authors.length; o++) {
         var el = element.authors[o]
-        authors += `<a target="_blank" href="${el[1]}">${el[0]}</a> `
+        if (element.authors.length !== 1) {
+          if (o === element.authors.length - 1) {
+            if (el[1] === '') {
+              authors += el[0]
+            } else authors += `<a target="_blank" href="${el[1]}">${el[0]}</a>`
+          } else authors += `<a target="_blank" href="${el[1]}">${el[0]}</a>, `
+        } else {
+          if (el[1] === '') {
+            authors += el[0]
+          } else authors += `<a target="_blank" href="${el[1]}">${el[0]}</a>`
+        }
       }
       td5.innerHTML = authors
       tr.appendChild(td5)
 
-      // Append to tbody.
+      // Append 'tr' to tbody.
       tbody.appendChild(tr)
     }
   }
